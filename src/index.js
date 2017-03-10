d26d354c603b63647b21e6d53cc7e32d3fd24e62
Vivek Poddar <vivekimsit@gmail.com>
2017-03-10 09:46:43 +0530

    Init.

---
+++ src/index.js
@@ -0,0 +1,28 @@
+import 'babel-polyfill';
+import React from 'react';
+import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import throttle from 'lodash/throttle';
+import todoApp from './reducers';
+import App from './components/App';
+import { loadState, saveState } from './localStorage';
+
+const persistedState = loadState();
+const store = createStore(
+  todoApp,
+  persistedState
+);
+
+store.subscribe(throttle(() => {
+  saveState({
+    todos: store.getState().todos,
+  });
+}, 1000));
+
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
